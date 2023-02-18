526d0d9f268c2ebf16f5e1e111ebe15a1e7a684d
André Buitoni <a.buitoni@gmail.com>
2023-02-18 03:52:36 -0300

    Initial commit of newsletter-project

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+// const request = require("request") --> This might be needed in the future.
+// const https = require("https") --> This might be needed in the future.
+
+// The WebDeb Course that I'm following is a bit old and this is why I'm not sure that everything will be needed
+
+const app = express()
+
+app.use(express.static(__dirname));
+// app.use(express.static("public")); --> This might be needed at some point.
+// This is how Angela has done, but I had to change it to __dirname to make it work properly.
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+
+    var firstName = req.body.fName
+    var lastName = req.body.lName
+    var email = req.body.email
+
+    console.log(firstName, lastName, email);
+})
+
+app.listen(3000, function() {
+    console.log("Server Running on port 3000")
+})
